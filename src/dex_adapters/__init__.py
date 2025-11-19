61465202979b55d241ec3f35c9cf606da548ea7f
Luca Knaack <l.knaack93@gmail.com>
2025-11-19 22:42:54 +0100

    feat: add ExtendedAdapter and related configuration, update models and example usage

--- src/dex_adapters/__init__.py
+++ src/dex_adapters/__init__.py
@@ -1,4 +1,5 @@
+from .extended_adapter import ExtendedAdapter
 from .hyperliquid import HyperliquidAdapter
 from .lighter_adapter import LighterAdapter
 
-__all__ = ["HyperliquidAdapter", "LighterAdapter"]
+__all__ = ["ExtendedAdapter", "HyperliquidAdapter", "LighterAdapter"]
